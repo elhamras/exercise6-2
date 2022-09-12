b3c2575451686e6789a5646db4c87cacabcf5a2b
exercise3-2 <elirasm86@gmail.com>
2022-09-12 10:37:26 +0430

    ecersice5--1

--- exercise5-1.py
+++ exercise5-1.py
@@ -2,7 +2,7 @@ import random
 dicelist = [ ]
 x = 1
 count = int(input("enter the dice rolling count: "))
-for x in range(1,count):
+for x in range(count):
     dice = random.randint(1,6)
     dicelist.append(dice)
 print(dicelist)
